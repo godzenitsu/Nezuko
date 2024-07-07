d262a1706dc9abaeb7096eec728409ad7beae886
GOD ZENITSU <164893015+godzenitsu@users.noreply.github.com>
2024-07-07 08:22:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ async function downloadSessionData() {
         console.error('Please add your session to SESSION_ID env !!');
         process.exit(1);
     }
-    const sessdata = config.SESSION_ID.split("queen-nezuko~&")[1];
+    const sessdata = config.SESSION_ID.split("Queen-Nezuko~")[1];
     const url = `https://pastebin.com/raw/${sessdata}`;
     try {
         const response = await axios.get(url);
