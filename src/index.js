54d2e96126c2ddfcadda16145342f552c4655f20
GOD ZENITSU <164893015+godzenitsu@users.noreply.github.com>
2024-07-10 19:32:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ async function start() {
             version,
             logger: pino({ level: 'silent' }),
             printQRInTerminal: true,
-            browser: Browsers.macOS("Safari"),
+            browser: ["NEZUKO-MD", "safari", "3.3"],
             auth: state,
             getMessage: async (key) => {
                 if (store) {
