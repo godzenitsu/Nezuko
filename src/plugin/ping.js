888b95e450c9fd23b4d49bdcaeaff55826265185
GOD ZENITSU <164893015+godzenitsu@users.noreply.github.com>
2024-07-11 20:22:36 +0530

    Update ping.js

--- src/plugin/ping.js
+++ src/plugin/ping.js
@@ -4,11 +4,11 @@ const ping = async (m, sock) => {
 
   if (cmd === "ping") {
     const start = new Date().getTime();
-    await m.React('⚡');
+    await m.React('📡');
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
 
-    const text = `*_🔥⃝вσт ѕρєє∂: ${responseTime.toFixed(2)} s_*`;
+    const text = `*ʟᴀᴛᴇɴᴄʏ: ${responseTime.toFixed(2)} ms_*`;
     sock.sendMessage(m.from, { text }, { quoted: m });
   }
 }
